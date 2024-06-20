f27e01095b0b51056f088e702e3adda6cf67bd73
Kochangwoo3 <love134@bible.ac.kr>
2024-06-20 19:34:31 +0900

    Add files via upload

---
+++ chapter2/CharacterEx2.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class CharacterEx2 {
+
+	public static void main(String[] args) {
+//		char ch1 = '한';
+//		char ch2 = '\uD55C';
+		//char ch3 = -65;
+		char ch1 = 'Z';
+		char ch2 = 38;
+		char ch3 = 97;
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+	}
+
+}
---
+++ chapter2/IntegerVariable.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class IntegerVariable {
+	public static void main(String[] ar) {
+		short sVal = 10;
+		byte bVal = 20;
+		
+		int sum = sVal + bVal;
+		long lVal = 12345678900L;
+		
+		//System.out.println(sVal + bVal);
+		System.out.println(sum);
+	}
+}
